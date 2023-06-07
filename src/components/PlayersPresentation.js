e18fb44339944c09ef817b346bdba990aa820f00
tuananh <letuananh2663@gmail.com>
2023-06-07 23:13:10 +0700

    State Hook

--- src/components/PlayersPresentation.js
+++ src/components/PlayersPresentation.js
@@ -1,17 +1,32 @@
 import React from 'react'
-export default function PlayersPresentation({players}) {
-     //console.log(players);
+import { useState } from 'react'
+export default function PlayersPresentation({ players }) {
+    const [player, setPlayer] = useState([])
+    //console.log(players);
     return (
-     <div className='container'>
-        {players.map((player)=>(
-        <div className='column'>
-        <div className='card'>
-        <img src={player.img}/>
-          <h3>{player.name}</h3>
-          <p className='title'>{player.club}</p>
-          <p><button>Detail</button></p>
-        </div>
-      </div>
-     ))}</div>   
-  )
+        <>
+            <div className='container'>
+                {players.map((player) => (
+                    <div className='column'>
+                        <div className='card'>
+                            <img src={player.img} alt='' />
+                            <h3>{player.name}</h3>
+                            <p className='title'>{player.club}</p>
+                            <p><button onClick={() => { setPlayer(player) }}><a href='#popup1' id='openPopUp'>Detail</a></button></p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div id='popup1' className='overlay'>
+                <div className='popup'>
+                    <img src={player.img} alt='' />
+                    <h2>{player.name}</h2>
+                    <a className='close' href='#home'>&times;</a>
+                    <div className='content'>
+                        {player.info}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
